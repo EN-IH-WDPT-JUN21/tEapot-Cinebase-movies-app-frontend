b08bec6f4a39d81aac7c296f2369e9b74b14a50b
mat.poreda <mat.poreda@gmail.com>
2021-11-13 11:59:29 +0100

    implemented basic security with auth0 login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from 'src/app/pages/profile/profile.component';
 
-const routes: Routes = [];
+
+import { AuthGuard } from '@auth0/auth0-angular';
+
+const routes: Routes = [
+
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuard],
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,15 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import packageInfo from '../../auth_config.json';
+
 export const environment = {
-  production: false
+  production: false,
+  auth: {
+    domain: packageInfo.domain,
+    clientId: packageInfo.clientId,
+    redirectUri: window.location.origin,
+  },
 };
 
 /*
